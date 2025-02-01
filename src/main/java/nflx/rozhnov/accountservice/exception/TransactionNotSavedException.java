778cc5772bd5194a6c8cb11ad84b3c5d7707f83d
lrozhnov <lrozhnov@neoflex.ru>
2025-02-01 18:21:39 +0300

    fix exception comment

--- src/main/java/nflx/rozhnov/accountservice/exception/TransactionNotSavedException.java
+++ src/main/java/nflx/rozhnov/accountservice/exception/TransactionNotSavedException.java
@@ -2,6 +2,6 @@ package nflx.rozhnov.accountservice.exception;
 
 public class TransactionNotSavedException extends RuntimeException {
     public TransactionNotSavedException(String msg) {
-        super("Ошибка сохранения транзакции: " + msg);
+        super("Error saving the transaction: " + msg);
     }
 }
